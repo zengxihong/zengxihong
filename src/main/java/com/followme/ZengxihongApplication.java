67de6421e39cbc608ff9e900bbc6f2909bb8796b
zengxihong <zengxihong2008@163.com>
2019-04-13 16:45:09 +0800

    Initial commit

---
+++ src/main/java/com/followme/ZengxihongApplication.java
@@ -0,0 +1,13 @@
+package com.followme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZengxihongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZengxihongApplication.class, args);
+	}
+
+}
